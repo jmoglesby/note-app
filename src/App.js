1e27f6db227f82d893eca71607b97e488467bae9
Jeremy Oglesby <iaminaband@gmail.com>
2019-10-13 22:46:15 -0700

    Adds List component scaffold

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import Nav from './components/Nav';
+import List from './components/List';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
         <Nav />
+        <List />
       </div>
     );
   }
